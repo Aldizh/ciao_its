009ba69be0051e2f6b98cdcfebbd725524e3ad85
Aldi <azhupani390@berkeley.edu>
2013-04-25 13:17:21 -0700

    fixed the default user and ticket status configuration in the form

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,6 @@ class Ticket < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   belongs_to :ticket_status
-  validates_presence_of :title, :description, :user_id, :category_id, :ticket_status_id
+  validates_presence_of :user, :title, :description, :category_id, :ticket_status_id
     
 end
